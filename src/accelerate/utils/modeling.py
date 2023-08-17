b1307e5b80fb2c322c373c8c5539470c1d388231
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2023-08-17 11:57:20 +0530

    Update modeling.py

--- src/accelerate/utils/modeling.py
+++ src/accelerate/utils/modeling.py
@@ -159,7 +159,8 @@ def set_module_tensor_to_device(
             value = value.to(old_value.dtype)
         elif not str(value.dtype).startswith(("torch.uint", "torch.int", "torch.bool")):
             value = value.to(dtype)
-
+    if is_xpu_available() and torch.xpu.get_device_properties(d).dev_type == "gpu":
+        device = torch.device(f"xpu:{device}")
     with torch.no_grad():
         if value is None:
             if is_xpu_available() and device != "cpu":
